867f44ad12a0bb54b0625cf4dbd6a01581c6afdf
Dhanraj297 <you@example.com>
2026-04-29 16:16:59 +0530

    fbr

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(supabaseUrl, supabaseKey)
